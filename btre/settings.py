98055b050adafdcf118aeadef2f479006ee8cb71
Sangam Singh <sangsing126@gmail.com>
2019-10-03 16:56:59 +0530

    second commit

--- btre/settings.py
+++ btre/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pages.apps.PagesConfig', 
+    'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',  
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'), 
-]
+] 
